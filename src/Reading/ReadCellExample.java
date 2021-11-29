9fbe07b586939871114d44f64e8c5d705577a17c
DONG HUNG FENG <donghung.feng@gmail.com>
2021-11-29 16:43:44 +0700

    second commit

--- src/Reading/ReadCellExample.java
+++ src/Reading/ReadCellExample.java
@@ -15,21 +15,18 @@ public class ReadCellExample {
     private static int col = 0;
     public static void main(String[] args)
     {
-        ReadCellExample rc=new ReadCellExample();   //object of the class
-        //reading the value of 2nd row and 2nd column
+        ReadCellExample rc=new ReadCellExample();
         String vOutput=rc.ReadCellData(row, col);
         System.out.println(vOutput);
     }
     //method defined for reading a cell
     public String ReadCellData(int vRow, int vColumn)
     {
-        String value=null;          //variable for storing the cell value
-        Workbook wb=null;           //initialize Workbook null
+        String value=null;
+        Workbook wb=null;
         try
         {
-            //reading data from a file in the form of bytes
             FileInputStream fis=new FileInputStream(fileName);
-            //constructs an XSSFWorkbook object, by buffering the whole stream into the memory
             wb=new XSSFWorkbook(fis);
         }
         catch(FileNotFoundException e)
@@ -40,10 +37,10 @@ public class ReadCellExample {
         {
             e1.printStackTrace();
         }
-        Sheet sheet=wb.getSheetAt(0);   //getting the XSSFSheet object at given index
-        Row row=sheet.getRow(vRow); //returns the logical row
-        Cell cell=row.getCell(vColumn); //getting the cell representing the given column
-        value=cell.getStringCellValue();    //getting cell value
-        return value;               //returns the cell value
+        Sheet sheet=wb.getSheetAt(0);
+        Row row=sheet.getRow(vRow);
+        Cell cell=row.getCell(vColumn);
+        value=cell.getStringCellValue();
+        return value;        
     }
 }
